354c967c493048d7fa5ce7e36f93c49bd7682217
Hilary Titus Naor <hilarytn@gmail.com>
2023-06-07 14:35:48 +0000

    Add columns

--- models/base_model.py
+++ models/base_model.py
@@ -2,12 +2,17 @@
 import uuid
 from datetime import datetime
 from models import storage
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 
 class BaseModel:
 
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Columm()
-    updated_at = 
+    created_at = Column(DateTime nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
@@ -21,13 +26,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -36,4 +41,6 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
+        if new_dict.get('_sa_instance_state'):
+            del new_dict['_sa_instance_state']
         return new_dict
