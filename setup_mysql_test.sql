022814372b25e69917f7471b2c0882b07323ad9f
Hilary Titus Naor <hilarytn@gmail.com>
2023-06-08 15:31:42 +0000

    remove sql scripts

--- setup_mysql_test.sql
+++
@@ -1,7 +0,0 @@
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS linky_test_db;
-CREATE USER  IF NOT EXISTS 'linky_test'@'localhost' IDENTIFIED BY 'linky_dev_pwd@June2023';
-GRANT ALL PRIVILEGES ON `linky_test_db`.* TO 'linky_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'linky_test'@'localhost';
-FLUSH PRIVILEGES;
